6ed96927f80a823a6497832828bb48966c09ce16
diamond95 <ivanvk95@gmail.com>
2019-10-10 18:44:46 +0200

    updated

--- src/Component/ViewComments.js
+++ src/Component/ViewComments.js
@@ -9,7 +9,7 @@ export default class AddModal extends React.Component {
     render() {
         return(
           
-            <Modal key={this.props.id}
+            <Modal key={this.props.modelId}
             {...this.props}
             size="lg"
             aria-labelledby="contained-modal-title-vcenter"
@@ -17,14 +17,19 @@ export default class AddModal extends React.Component {
           >
             <Modal.Header closeButton>
               <Modal.Title id="contained-modal-title-vcenter">
-                Komentari za objavu {this.props.id}.
+                Komentari za objavu {this.props.modelId}.
               </Modal.Title>
             </Modal.Header>
             <Modal.Body>
+              
               <p>
-                Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-                dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-                consectetur ac, vestibulum at eros.
+               {this.props.modelData.email}
+              </p>
+              <p>
+               {this.props.modelData.name}
+              </p>
+              <p>
+               {this.props.modelData.body}
               </p>
             </Modal.Body>
             <Modal.Footer>
--- src/Getdata.js
+++ src/Getdata.js
@@ -9,125 +9,137 @@ import Table from '@material-ui/core/Table';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
 
-import {Button, ButtonToolbar} from 'react-bootstrap';
+import { Button, ButtonToolbar } from 'react-bootstrap';
 import ViewComments from './Component/ViewComments';
 
 
 const StyledTableCell = withStyles(theme => ({
-    head: {
-      backgroundColor: theme.palette.common.black,
-      color: theme.palette.common.white,
-    },
-    body: {
-      fontSize: 14,
-    },
-  }))(TableCell);
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
 
-  const StyledTableRow = withStyles(theme => ({
-    root: {
-      '&:nth-of-type(odd)': {
-        backgroundColor: theme.palette.background.default,
-      },
+const StyledTableRow = withStyles(theme => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.background.default,
     },
-  }))(TableRow);
+  },
+}))(TableRow);
 
-  const useStyles = makeStyles(theme => ({
-    button: {
-      margin: theme.spacing(1),
-    },
-  }));
-  
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
+
 
 class App extends React.Component {
 
 
-    
-    constructor(props) {
-        
-      super(props);
-      this.state = {
-        items: [],
-        isLoaded: false,
-        addModalShow: false,
-      }
-    }
 
-    openModal (id) {
-      this.setState({
-        addModalShow: {
-            [id]: true
-         }
+  constructor(props) {
+
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+      addModalShow: false,
+      modelData: [],
+      modelId: null
+    }
+  }
+
+  openModal(id) {
+    this.setState({
+      addModalShow: {
+        [id]: true
+      }
+    });
+  }
+
+  componentDidMount() {
+
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json,
+        })
       });
-   }
 
-    componentDidMount() {
+  }
 
-      fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(res => res.json())
-        .then(json => {
-          this.setState({
-            isLoaded: true,
-            items: json,
-          })
-        });
 
+  openModel(id) {
+
+    fetch(`https://jsonplaceholder.typicode.com/comments/${id}`)
+    .then(res => res.json())
+    .then(json => {
+      this.setState({
+      modelData: json
+      })
+    });
+    this.setState({ addModalShow: [id],  modelId: id})
+  }
+  render() {
+
+    var { isLoaded, items } = this.state;
+    var addModalClose = () => this.setState({ addModalShow: false });
+    if (!isLoaded) {
+      return <div className="center"><CircularProgress disableShrink /></div>;
     }
 
-    
 
-    render() {
 
-      var { isLoaded, items } = this.state;
-      var addModalClose = () => this.setState({addModalShow: false});
-      if(!isLoaded) {
-        return <div className="center"><CircularProgress disableShrink /></div>;
-      }
+    else {
 
-     
-      
-      else {
+      return (
+        <div className="App">
 
-        return (
-          <div className="App">
-
-            <Table >
+          <Table >
             <TableHead>
-                <TableRow>
-                    <StyledTableCell>ID</StyledTableCell>
-                    <StyledTableCell align="center">Akcije</StyledTableCell>
-                    <StyledTableCell align="center">Naslov</StyledTableCell>
-                    <StyledTableCell align="center">Opis</StyledTableCell>
-                </TableRow>
+              <TableRow>
+                <StyledTableCell>ID</StyledTableCell>
+                <StyledTableCell align="center">Akcije</StyledTableCell>
+                <StyledTableCell align="center">Naslov</StyledTableCell>
+                <StyledTableCell align="center">Opis</StyledTableCell>
+              </TableRow>
             </TableHead>
             <TableBody>
-                {items.map(item => (
-                    <StyledTableRow key={item.id}>
-                    <StyledTableCell component="th" scope="row" >
-                        {item.id}.
+              {items.map(item => (
+                <StyledTableRow key={item.id}>
+                  <StyledTableCell component="th" scope="row" >
+                    {item.id}.
                     </StyledTableCell>
-                    <StyledTableCell>
-                      <ButtonToolbar>
-                      <Button variant='primary'   onClick={() => this.setState({addModalShow: [item.id]})}>
+                  <StyledTableCell>
+                    <ButtonToolbar>
+                      <Button variant='primary' onClick={() => this.openModel(item.id)}>
                         Komentari
                       </Button>
-                      </ButtonToolbar>
+                    </ButtonToolbar>
 
-                    </StyledTableCell>
-                    <StyledTableCell align="left" className="tekst">{item.title}</StyledTableCell>
-                    <StyledTableCell align="center" className="tekst">{item.body}</StyledTableCell>
-                    </StyledTableRow>
-                ))}
-                <ViewComments  show={this.state.addModalShow} onHide={addModalClose} />
+                  </StyledTableCell>
+                  <StyledTableCell align="left" className="tekst">{item.title}</StyledTableCell>
+                  <StyledTableCell align="center" className="tekst">{item.body}</StyledTableCell>
+                </StyledTableRow>
+              ))}
+              <ViewComments show={this.state.addModalShow} onHide={addModalClose} modelId={this.state.modelId} modelData={this.state.modelData}/>
             </TableBody>
-            </Table>
-          </div>
-          
-        );
-      }
-     
-      
+          </Table>
+        </div>
 
+      );
     }
+
+
+
+  }
 }
 
 export default App;
